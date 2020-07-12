05ec23e1d4e88f2289a79919b75cfd3ddb456d2e
Christal Vigil <c.vigil27@gmail.com>
2020-07-12 17:53:17 -0600

    updating

--- myscript.js
+++ myscript.js
@@ -27,70 +27,88 @@ var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
 
 
-//prompt for character lenth, if/else statement  --need to figure out for all below how to allow user to enter character length or accept special, numerical, etc. if not chosen correctly 
+//function & prompt for character lenth, if/else statement 
+function getPassword () {
+    var charLength = prompt("Choose password character length of 8 or more characters but no greater than 128 characters?");
+    var charNum = parseInt(charLength); 
+    console.log("number of characters", charNum); 
+    console.log("truth condition met?", charNum <8 || charNum >128);
 
-var charLength = prompt("Choose password character length of 8 or more characters but no greater than 128 characters?")
-var charNum = parseInt(charLength); 
-console.log("number of characters", charNum); 
-console.log("truth condition met?", charNum <8 || charNum >128); 
-
-
-if (charNum <8 || charNum >128){ 
-    alert("Password criteria not met, choose length between 8 & 128 characters"); 
-    console.log("Password criteria is not met");
-}
-    else {
+    if (charNum <8 || charNum >128){ 
+        alert("Password criteria not met, choose length between 8 & 128 characters"); 
+        getPassword();
+    } else {
         alert("Password criteria met.");  
-    } 
-
-
-//confirm for special character, if/else statement 
-
-var hasSpecial = confirm ("Click OK to confirm including special characters?"); 
-
-if (hasSpecial){
-    console.log("Special characters included in password generator"); 
-}
-else {
-        alert("Choose OK to include special characters");   
+    }    
 }
 
+getPassword(); 
 
-//confirm for numerical character, if/else statement 
+//function & confirm for special character, if/else statement 
 
-var hasNumbers = confirm ("Click OK to confirm including numerical characters?"); 
+function getSpecial () {
+    var hasSpecial = confirm ("Click OK to confirm including special characters?"); 
 
-if (hasNumbers){
-    console.log("Numerical characters included in password generator");  
+    if (hasSpecial){
+        console.log("Special characters included in password generator");  
+    } else {
+        alert("Choose OK to include special characters"); 
+        getSpecial();   
+    }
 }
-    else {
+
+getSpecial(); 
+
+
+
+//function & confirm for numerical character, if/else statement 
+
+function getNumbers () { 
+    var hasNumbers = confirm ("Click OK to confirm including numerical characters?"); 
+
+    if (hasNumbers){
+        console.log("Numerical characters included in password generator");  
+    } else {
         alert("Choose OK to include numerical characters"); 
+        getNumbers();  
     } 
+}
+
+getNumbers(); 
 
 
 
 //confirm for lower case character, if/else statement 
 
-var hasLow = confirm ("Click OK to confirm including lowcase characters?"); 
+function getLow () {
+    var hasLow = confirm ("Click OK to confirm including lowcase characters?"); 
 
-if (hasLow){
-    console.log("Lowcase characters included in password generator");  
-}
-    else {
+    if (hasLow){
+        console.log("Lowcase characters included in password generator");  
+    } else {
         alert("Choose OK to include lowcase characters."); 
+        getLow (); 
     }
+}
 
+getLow (); 
 
 
 //confirm for uppercase character, if/else statements 
 
-var hasUpper = confirm ("Click OK to confirm including uppercase characters?");
+function getUpper () {
+    var hasUpper = confirm ("Click OK to confirm including uppercase characters?");
 
-if (hasUpper){
-    console.log("Uppercase characters will be included in password generator"); 
-}
-else {
-        alert("Choose OK to include uppercase characters."); 
+    if (hasUpper){
+        console.log("Uppercase characters will be included in password generator"); 
+    } else {
+        alert("Choose OK to include uppercase characters.");
+        getUpper (); 
+    }   
 }
 
+getUpper(); 
+
+
+//generate random password  
 
