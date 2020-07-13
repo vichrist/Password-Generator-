78f1a2983150c541d5392b1ca88a5e2c2eccfc32
Christal Vigil <c.vigil27@gmail.com>
2020-07-12 19:11:56 -0600

    updating code for password generator

--- myscript.js
+++ myscript.js
@@ -17,6 +17,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 //my arrays 
+var possChar = []
 
 var specialChar = ["~", "`","!","@","#","$","%","^","&","*","(",")","_", "-","+","=","{","}","[", "]","|"]; 
 
@@ -26,11 +27,12 @@ var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","
 
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]; 
 
+var charNum;
 
 //function & prompt for character lenth, if/else statement 
 function getPassword () {
     var charLength = prompt("Choose password character length of 8 or more characters but no greater than 128 characters?");
-    var charNum = parseInt(charLength); 
+    charNum = parseInt(charLength); 
     console.log("number of characters", charNum); 
     console.log("truth condition met?", charNum <8 || charNum >128);
 
@@ -44,16 +46,27 @@ function getPassword () {
 
 getPassword(); 
 
-//function & confirm for special character, if/else statement 
+//function & confirm for special character, if/else statement, for-looping and pushing  
 
 function getSpecial () {
     var hasSpecial = confirm ("Click OK to confirm including special characters?"); 
 
     if (hasSpecial){
         console.log("Special characters included in password generator");  
+    //add specialChar array to possChar array
+        // var possChar = []
+        // var specChar = [!,@,#,$]
+
+    // concatention -> adding strings (or others) together (look up .concat())
+        // possChar = possChar + specChart
+
+    // loop and push
+        for (char in specialChar) {
+            possChar.push(char);
+            console.log("logging specialChar");
+        }
     } else {
-        alert("Choose OK to include special characters"); 
-        getSpecial();   
+        alert("password will not contain special characters");    
     }
 }
 
@@ -67,10 +80,13 @@ function getNumbers () {
     var hasNumbers = confirm ("Click OK to confirm including numerical characters?"); 
 
     if (hasNumbers){
-        console.log("Numerical characters included in password generator");  
+        console.log("Numerical characters included in password generator"); 
+        for (charNum in numbers) {
+            possChar.push(charNum);
+            console.log("logging numberChar");
+        } 
     } else {
-        alert("Choose OK to include numerical characters"); 
-        getNumbers();  
+        alert("Password will not include numerical characters");   
     } 
 }
 
@@ -84,10 +100,13 @@ function getLow () {
     var hasLow = confirm ("Click OK to confirm including lowcase characters?"); 
 
     if (hasLow){
-        console.log("Lowcase characters included in password generator");  
+        console.log("Lowcase characters included in password generator");      
+        for (charLow in lowCase) {
+            possChar.push(charLow);
+            console.log("logging lowChar");
+        }
     } else {
-        alert("Choose OK to include lowcase characters."); 
-        getLow (); 
+        alert("Password will not include lowcase characters."); 
     }
 }
 
@@ -101,9 +120,12 @@ function getUpper () {
 
     if (hasUpper){
         console.log("Uppercase characters will be included in password generator"); 
+        for (charUpper in upperCase) {
+            possChar.push(charUpper);
+            console.log("logging upperChar");
+        }
     } else {
-        alert("Choose OK to include uppercase characters.");
-        getUpper (); 
+        alert("Password will not include uppercase characters."); 
     }   
 }
 
@@ -112,3 +134,15 @@ getUpper();
 
 //generate random password  
 
+for (var i = 0; i < charNum ; i++) {
+    var num = Math.random(); 
+    var randomIndex = Math.floor(num * possChar.length); 
+    (possChar[num]); 
+} 
+
+
+    // generate random number
+    // var num = Math.random(); 0 -> 0.99999
+    // var randomIndex = Math.floor(num * possChar.length);
+    // produce a random element from the possChar array (possChar[num]);
+// }
